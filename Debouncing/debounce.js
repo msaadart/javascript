2b8d7349ab05605b59c4677480a187c77e441a0c
msaadart <saadnasir99@gmail.com>
2024-08-11 17:51:30 +0500

    What is Debouncing in JavaScript

---
+++ Debouncing/debounce.js
@@ -0,0 +1,32 @@
+function handler(){
+    // This function simply logs an API request message to the console, including the current value of the search input field. It simulates the action you would take when sending an API request.
+    console.log(`API request: ${searchInput.value}` );
+}
+
+// The searchInput variable is assigned the DOM element with the ID "search-input".
+const searchInput = document.getElementById("search-input");
+
+// An event listener is added to the searchInput element, which listens for the input event. This event fires every time the user types something in the input field.
+searchInput.addEventListener('input', debounce(handler, 500));
+
+// The debounce function takes two arguments: f, the function to be debounced (in this case, handler), and ms, the delay in milliseconds (500ms in this case).
+function debounce(f, ms){
+    // This variable keeps track of the timeout set for the delay
+    let timeout;
+
+    // The debounce function returns a new function
+    return function()
+    {
+        // If there is already a timeout in progress, it clears it. 
+        if(timeout){
+            clearTimeout(timeout);
+        }
+        
+        // It sets a new timeout that will execute the handler function after the specified delay (500ms) has passed since the last key press.
+        timeout = setTimeout(() => {
+            // it checks if the input field is not empty 
+            searchInput.value != '' && f();
+        }, ms);
+    }   
+}
+
